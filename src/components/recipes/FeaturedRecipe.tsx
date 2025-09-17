1853175ae83a920cadc978a645bd8ddb7ec342d6
taayyohh <milesxbridges@gmail.com>
2025-09-16 21:04:14 -0400

    build

--- src/components/recipes/FeaturedRecipe.tsx
+++ src/components/recipes/FeaturedRecipe.tsx
@@ -70,7 +70,7 @@ function FeaturedRecipeContent() {
         direction={{ s: "column", m: "row" }}
         height={{ s: "auto", m: "320px" }}
       >
-        {/* Recipe - Left Siiide */}
+        {/* Recipe - Left Side */}
         <View
           width={{ s: "100%", m: "50%" }}
           height="320px"
